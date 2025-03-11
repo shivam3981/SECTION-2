8b0b888e8dc77ab4f4f15c3ae19f6617a33dc431
shivam3981 <singhshivam112002@gmail.com>
2025-03-11 10:11:03 +0530

    Created Pages

---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+
+      <h1 className='text-4xl text-center font-bold'>Home Page</h1>
+
+    </div>
+  )
+}
+
+export default Home;
