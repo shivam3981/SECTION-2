778729c03d48ceab689d39d1a3b6fa6f5e9cb3b6
shivam3981 <singhshivam112002@gmail.com>
2025-03-26 00:49:12 +0530

    new

--- frontend/src/app/signup/page.jsx
+++ frontend/src/app/signup/page.jsx
@@ -1,54 +1,216 @@
+'use client';
+import { useFormik } from 'formik';
 import React from 'react'
 
 const Signup = () => {
+
+  const signupForm = useFormik({
+    initialValues: {
+      name : '',
+      email : '',
+      password : '',
+      ConfirmPassword : '' ,
+    },
+
+    onSubmit:(values)=>{
+      console.log(values);
+
+      // send values to backend 
+      
+    }
+
+  })
     return (
         <div>
-            <section className="text-gray-600 body-font">
-  <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
-    <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
-      <h1 className="title-font font-medium text-3xl text-gray-900">
-        Slow-carb next level shoindcgoitch ethical authentic, poko scenester
-      </h1>
-      <p className="leading-relaxed mt-4">
-        Poke slow-carb mixtape knausgaard, typewriter street art gentrify
-        hammock starladder roathse. Craies vegan tousled etsy austin.
+          <div className="mt-7 max-w-lg mx-auto bg-white border border-gray-200 rounded-xl shadow-2xs">
+  <div className="p-4 sm:p-7">
+    <div className="text-center">
+      <h1 className="block text-2xl font-bold text-gray-800">Sign up</h1>
+      <p className="mt-2 text-sm text-gray-600">
+        Already have an account?
+        <a
+          className="text-blue-600 decoration-2 hover:underline focus:outline-hidden focus:underline font-medium"
+          href="../examples/html/signin.html"
+        >
+          Sign in here
+        </a>
       </p>
     </div>
-    <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
-      <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
-        Sign Up
-      </h2>
-      <div className="relative mb-4">
-        <label htmlFor="full-name" className="leading-7 text-sm text-gray-600">
-          Full Name
-        </label>
-        <input
-          type="text"
-          id="full-name"
-          name="full-name"
-          className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-        />
-      </div>
-      <div className="relative mb-4">
-        <label htmlFor="email" className="leading-7 text-sm text-gray-600">
-          Email
-        </label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-        />
-      </div>
-      <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-        Button
+    <div className="mt-5">
+      <button
+        type="button"
+        className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-2xs hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
+      >
+        <svg
+          className="w-4 h-auto"
+          width={46}
+          height={47}
+          viewBox="0 0 46 47"
+          fill="none"
+        >
+          <path
+            d="M46 24.0287C46 22.09 45.8533 20.68 45.5013 19.2112H23.4694V27.9356H36.4069C36.1429 30.1094 34.7347 33.37 31.5957 35.5731L31.5663 35.8669L38.5191 41.2719L38.9885 41.3306C43.4477 37.2181 46 31.1669 46 24.0287Z"
+            fill="#4285F4"
+          />
+          <path
+            d="M23.4694 47C29.8061 47 35.1161 44.9144 39.0179 41.3012L31.625 35.5437C29.6301 36.9244 26.9898 37.8937 23.4987 37.8937C17.2793 37.8937 12.0281 33.7812 10.1505 28.1412L9.88649 28.1706L2.61097 33.7812L2.52296 34.0456C6.36608 41.7125 14.287 47 23.4694 47Z"
+            fill="#34A853"
+          />
+          <path
+            d="M10.1212 28.1413C9.62245 26.6725 9.32908 25.1156 9.32908 23.5C9.32908 21.8844 9.62245 20.3275 10.0918 18.8588V18.5356L2.75765 12.8369L2.52296 12.9544C0.909439 16.1269 0 19.7106 0 23.5C0 27.2894 0.909439 30.8731 2.49362 34.0456L10.1212 28.1413Z"
+            fill="#FBBC05"
+          />
+          <path
+            d="M23.4694 9.07688C27.8699 9.07688 30.8622 10.9863 32.5344 12.5725L39.1645 6.11C35.0867 2.32063 29.8061 0 23.4694 0C14.287 0 6.36607 5.2875 2.49362 12.9544L10.0918 18.8588C11.9987 13.1894 17.25 9.07688 23.4694 9.07688Z"
+            fill="#EB4335"
+          />
+        </svg>
+        Sign up with Google
       </button>
-      <p className="text-xs text-gray-500 mt-3">
-        Literally you probably haven't heard of them jean shorts.
-      </p>
+      <div className="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-1 before:border-t before:border-gray-200 before:me-6 after:flex-1 after:border-t after:border-gray-200 after:ms-6">
+        Or
+      </div>
+      {/* Form */}
+      <form onSubmit={ signupForm.handleSubmit}>
+        <div className="grid gap-y-4">
+          {/* Form Group */}
+          <div>
+            <label htmlFor="email" className="block text-sm mb-2">
+              Email address
+            </label>
+            <div className="relative">
+              <input
+                type="email"
+                id="email"
+                onChange={ signupForm.handleChange}
+                value={ signupForm.values.email }
+                className="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+                required=""
+                aria-describedby="email-error"
+              />
+              <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                <svg
+                  className="size-5 text-red-500"
+                  width={16}
+                  height={16}
+                  fill="currentColor"
+                  viewBox="0 0 16 16"
+                  aria-hidden="true"
+                >
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                </svg>
+              </div>
+            </div>
+            <p className="hidden text-xs text-red-600 mt-2" id="email-error">
+              Please include a valid email address so we can get back to you
+            </p>
+          </div>
+          {/* End Form Group */}
+          {/* Form Group */}
+          <div>
+            <label htmlFor="password" className="block text-sm mb-2">
+              Password
+            </label>
+            <div className="relative">
+              <input
+                type="password"
+                id="password"
+                onChange={ signupForm.handleChange}
+                value={ signupForm.values.password }
+                className="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+                required=""
+                aria-describedby="password-error"
+              />
+              <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                <svg
+                  className="size-5 text-red-500"
+                  width={16}
+                  height={16}
+                  fill="currentColor"
+                  viewBox="0 0 16 16"
+                  aria-hidden="true"
+                >
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                </svg>
+              </div>
+            </div>
+            <p className="hidden text-xs text-red-600 mt-2" id="password-error">
+              8+ characters required
+            </p>
+          </div>
+          {/* End Form Group */}
+          {/* Form Group */}
+          <div>
+            <label htmlFor="confirm-password" className="block text-sm mb-2">
+              Confirm Password
+            </label>
+            <div className="relative">
+              <input
+                type="password"
+                id="ConfirmPassword"
+                onChange={ signupForm.handleChange}
+                value={ signupForm.values.ConfirmPassword }
+                
+                className="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+                required=""
+                aria-describedby="confirm-password-error"
+              />
+              <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                <svg
+                  className="size-5 text-red-500"
+                  width={16}
+                  height={16}
+                  fill="currentColor"
+                  viewBox="0 0 16 16"
+                  aria-hidden="true"
+                >
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                </svg>
+              </div>
+            </div>
+            <p
+              className="hidden text-xs text-red-600 mt-2"
+              id="confirm-password-error"
+            >
+              Password does not match the password
+            </p>
+          </div>
+          {/* End Form Group */}
+          {/* Checkbox */}
+          <div className="flex items-center">
+            <div className="flex">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                className="shrink-0 mt-0.5 border-gray-200 rounded-sm text-blue-600 focus:ring-blue-500"
+              />
+            </div>
+            <div className="ms-3">
+              <label htmlFor="remember-me" className="text-sm">
+                I accept the{" "}
+                <a
+                  className="text-blue-600 decoration-2 hover:underline focus:outline-hidden focus:underline font-medium"
+                  href="#"
+                >
+                  Terms and Conditions
+                </a>
+              </label>
+            </div>
+          </div>
+          {/* End Checkbox */}
+          <button
+            type="submit"
+            className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+          >
+            Sign up
+          </button>
+        </div>
+      </form>
+      {/* End Form */}
     </div>
   </div>
-</section>
+</div>
 
         </div>
     )
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 
 const Navbar = () => {
@@ -8,57 +9,14 @@ const Navbar = () => {
                 <header className="flex flex-wrap  md:justify-start md:flex-nowrap z-50 w-full bg-white border-b border-gray-200 dark:bg-neutral-800 dark:border-neutral-700">
                     <nav className="relative max-w-[85rem] w-full mx-auto md:flex md:items-center md:justify-between md:gap-3 py-2 px-4 sm:px-6 lg:px-8">
                         <div className="flex justify-between items-center gap-x-1">
-                            <a
+                            <Link
                                 className="flex-none font-semibold text-2xl text-black focus:outline-hidden focus:opacity-80 dark:text-white"
-                                href="#"
+                                href="/"
                                 aria-label="Ease Mail.ai"
                             >
                                 Ease Mail.ai
-                            </a>
-                            {/* Collapse Button */}
-                            <button
-                                type="button"
-                                className="hs-collapse-toggle md:hidden relative size-9 flex justify-center items-center font-medium text-sm rounded-lg border border-gray-200 text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-                                id="hs-header-base-collapse"
-                                aria-expanded="false"
-                                aria-controls="hs-header-base"
-                                aria-label="Toggle navigation"
-                                data-hs-collapse="#hs-header-base"
-                            >
-                                <svg
-                                    className="hs-collapse-open:hidden size-4"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width={24}
-                                    height={24}
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeWidth={2}
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                >
-                                    <line x1={3} x2={21} y1={6} y2={6} />
-                                    <line x1={3} x2={21} y1={12} y2={12} />
-                                    <line x1={3} x2={21} y1={18} y2={18} />
-                                </svg>
-                                <svg
-                                    className="hs-collapse-open:block shrink-0 hidden size-4"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width={24}
-                                    height={24}
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeWidth={2}
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                >
-                                    <path d="M18 6 6 18" />
-                                    <path d="m6 6 12 12" />
-                                </svg>
-                                <span className="sr-only">Toggle navigation</span>
-                            </button>
-                            {/* End Collapse Button */}
+                            </Link>
+                        
                         </div>
                         {/* Collapse */}
                         <div
@@ -70,28 +28,13 @@ const Navbar = () => {
                                 <div className="py-2 md:py-0  flex flex-col md:flex-row md:items-center gap-0.5 md:gap-1">
                                     <div className="grow">
                                         <div className="flex flex-col md:flex-row md:justify-end md:items-center gap-0.5 md:gap-1">
-                                            <a
+                                            <Link
                                                 className="p-2 flex items-center text-sm bg-gray-100 text-gray-800 hover:bg-gray-100 rounded-lg focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-700 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-                                                href="#"
+                                                href="/login"
                                                 aria-current="page"
                                             >
-                                                <svg
-                                                    className="shrink-0 size-4 me-3 md:me-2 block md:hidden"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    width={24}
-                                                    height={24}
-                                                    viewBox="0 0 24 24"
-                                                    fill="none"
-                                                    stroke="currentColor"
-                                                    strokeWidth={2}
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                >
-                                                    <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
-                                                    <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-                                                </svg>
-                                                Landing
-                                            </a>
+                                                Login
+                                            </Link>
                                             {/* Dropdown */}
                                             <div className="hs-dropdown [--strategy:static] md:[--strategy:fixed] [--adaptive:none] [--is-collapse:true] md:[--is-collapse:false] ">
                                                 <button
